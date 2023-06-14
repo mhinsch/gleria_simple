6886e30799694d361283a27cd31005b6938121ff
Martin Hinsch <hinsch.martin@gmail.com>
2023-06-14 22:25:05 +0100

    fix run_cmdl.jl

--- run_cmdl.jl
+++ run_cmdl.jl
@@ -15,96 +15,84 @@
 
 
 
+include("main_util.jl")
+
+add_to_load_path!("./lib/")
+
+
+using ParamUtils
+
+include("params.jl")
 include("setup.jl")
 
 
-
-### run simulation with given setup and parameters
-
-
-function run_events(sim, t_stop, logfile)
+function main()
+	
+	# *** parameters and command line
+	
+	# this creates parameters with default values as defined in params.jl,
+	# then overrides that with values that are provided as cmdl args
+    pars, args = load_parameters(ARGS, Params, 
+		["--rand-seed", "-r"],
+		Dict(
+			:help => "random seed",
+			:arg_type => Int,
+			:default => 42),
+		["--stop-time", "-t"],
+		Dict(
+			:help => "at which time to stop the simulation",
+			:arg_type => Float64, 
+			:default => 0.0)
+		)
+		
+	# end of sim
+	t_stop = args[:stop_time]
+		
+    # *** set up model 
+        
+	sim = setup(pars, args[:rand_seed])
 	model = sim.model
+	
+	# *** output, graphs
+	
+	logfile = prepare_outfiles("log_file.txt")	
+		
+	# *** main loop
+	
+	init_events(sim)
+	
 	t = 1.0
 	step = 1.0
-	last = 0
+	last_observe = 0
 
 	while t_stop <= 0 || t < t_stop
 		step_until!(sim, t) # run internal scheduler up to the next time step
 		
 		# we want the analysis to happen at every integral time step
-		if (now = trunc(Int, t)) >= last
+		if (now = trunc(Int, t)) >= last_observe
 			# in case we skipped a step (shouldn't happen, but just in case...)
-			for i in last:now
+			for i in last_observe:now
 				# print all stats to file
 				data = observe(Data, model, i)
 				log_results(logfile, data)
 			end
 			# remember when we did the last data output
-			last = now
+			last_observe = now
 		end
 
 		t += step
 
 #		println(t)
 	end
+	
+	close(logfile)
 end
 
 
 
-### setup, run, cleanup
-
-
-
-## parameters
-
-# parse command line args
-using ArgParse 
-# translate params to args and vice versa
-using Params2Args
-
-const arg_settings = ArgParseSettings("run simulation", autofix_names=true)
-
-@add_arg_table! arg_settings begin
-	"--rand-seed", "-r"
-		help = "random seed"
-		arg_type = Int
-		default = 42
-	"--stop-time", "-t"
-		help = "at which time to stop the simulation" 
-		arg_type = Float64 
-		default = 0.0
+if ! isinteractive()
+	@time main()
 end
 
-# new group of arguments
-add_arg_group!(arg_settings, "simulation parameters")
 
-# translate Params into args
-include("params.jl")
-fields_as_args!(arg_settings, Params)
-
-# parse cmdl args
-const args = parse_args(arg_settings, as_symbols=true)
-# and create a Params object from them
-const p = @create_from_args(args, Params)
-
-
-
-## setup
-
-const sim = setup(p, args[:rand_seed])
-
-const logf = prepare_outfiles("log_file.txt")
-
-
-
-## run
-
-init_events(sim)
-@time run_events(sim, args[:stop_time], logf)
-
-
-
-## cleanup
-
-close(logf)
 
